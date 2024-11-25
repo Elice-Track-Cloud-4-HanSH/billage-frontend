0bfa4ecb89c7f5be202d0bdd058a99bc2e774ac9
[CLOUD]나용진 <yjnaga@naver.com>
2024-11-25 17:30:26 +0900

    내 리뷰 페이지 기본 틀

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Easteregg from '@/Easteregg.jsx';
+import MyReview from './pages/MyReview';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/easter-egg' element={<Easteregg />} />
+        <Route path='/myreview' element={<MyReview/>} />
       </Routes>
     </BrowserRouter>
   );
