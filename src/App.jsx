da4feaacaf4cce9627c4923f84c095a7bb87c688
PromotezCitizen <gkstkdgus821@gmail.com>
2024-11-25 20:27:19 +0900

    feat: 전역으로 적용되는 layout 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Easteregg from '@/Easteregg.jsx';
+import Easteregg from '@/pages/Easteregg.jsx';
+import ChatroomList from '@/pages/ChatroomList.jsx';
+import '@/styles/global.css';
+import Layout from '@/layouts/Layout.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/easter-egg' element={<Easteregg />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/easter-egg' element={<Easteregg />} />
+          <Route path='/chat' element={<ChatroomList />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
