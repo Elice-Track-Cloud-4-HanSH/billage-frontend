977fc6311763839a46631240cd9a2297baaf39e8
PromotezCitizen <gkstkdgus821@gmail.com>
2024-11-25 14:21:05 +0900

    config: prettier 및 eslint 설정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vite.dev/config/
@@ -9,16 +9,16 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: "http://localhost:8080",
+        target: 'http://localhost:8080',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')  // /api prefix를 제거, 필요할 경우 다른 것으로 대체
-      }
-    }
+      },
+    },
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
-      '@': path.resolve(__dirname, 'src')  // '@'를 'src'로 매핑
-    }
-  }
-})
+      '@': path.resolve(__dirname, 'src'), // '@'를 'src'로 매핑
+    },
+  },
+});
