8b8eb5437caf0a024012234c3dea3b86a6c3697f
[CLOUD]나용진 <yjnaga@naver.com>
2024-11-27 04:14:11 +0900

    대여기록 페이지 기본 틀

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ChatroomList from '@/pages/ChatroomList.jsx';
 import '@/styles/global.css';
 import Layout from '@/layouts/Layout.jsx';
 import MyReview from '@/pages/MyReview';
+import MySales from './pages/MySales';
+import MyPurchase from './pages/MyPurchase';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
           <Route path='/easter-egg' element={<Easteregg />} />
           <Route path='/chat' element={<ChatroomList />} />
           <Route path='/myreview' element={<MyReview />} />
+          <Route path='/mysales' element={<MySales />} />
+          <Route path='/mypurchase' element={<MyPurchase />} />
         </Route>
       </Routes>
     </BrowserRouter>
