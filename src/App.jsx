aa16d9c6ea12deda2974e39a7c8b58f61be0ea3f
[CLOUD]나용진 <yjnaga@naver.com>
2024-11-27 16:18:14 +0900

    merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Layout from '@/layouts/Layout.jsx';
 import MyReview from '@/pages/MyReview';
 import MySales from './pages/MySales';
 import MyPurchase from './pages/MyPurchase';
+import ProductRegister from '@/pages/product/ProductRegister';
 import WriteProductReview from './pages/WriteProductReview';
-import WriteUserReview from './pages/WriteUserReview';
+import WriteUserReview from './pages/WriteUserReview.jsx';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path='/myreview' element={<MyReview />} />
           <Route path='/mysales' element={<MySales />} />
           <Route path='/mypurchase' element={<MyPurchase />} />
+          <Route path='/product-register' element={<ProductRegister />} />
           <Route path='/product-review/:id' element={<WriteProductReview />} />
           <Route path='user-review/:id' element={<WriteUserReview />} />
         </Route>
