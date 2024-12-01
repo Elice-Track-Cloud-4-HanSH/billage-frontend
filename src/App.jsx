c0b138387b076c888c0e11cf137044c821cb49f5
[CLOUD]나용진 <yjnaga@naver.com>
2024-12-01 11:18:42 +0900

    대여기록 생성 페이지 및 일부 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Easteregg from '@/pages/Easteregg.jsx';
 import ChatroomList from '@/pages/ChatroomList.jsx';
+import ChatPage from '@/pages/ChatPage';
 import '@/styles/global.css';
 import Layout from '@/layouts/Layout.jsx';
 import MyReview from '@/pages/MyReview';
 import MySales from './pages/MySales';
 import MyPurchase from './pages/MyPurchase';
-import ProductRegister from '@/pages/product/ProductRegister';
+import { ProductRegister, ProductEdit } from '@/pages/product/ProductUpsert.jsx';
 import WriteProductReview from './pages/WriteProductReview';
 import WriteUserReview from './pages/WriteUserReview.jsx';
-import Login from '@/pages/Login.jsx';  // 로그인 페이지 경로
-import Signup from '@/pages/Signup.jsx'; // 회원가입 페이지 경로
+import Login from '@/pages/Login.jsx';
+import Signup from '@/pages/Signup.jsx';
 import ProductDetail from '@/pages/product/ProductDetail';
+import Logout from '@/pages/Logout.jsx';
+import ProfileEdit from '@/pages/ProfileEdit.jsx';
+import SetToRented from '@/pages/rental-record/SetToRented';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
+          {/* 채팅 */}
           <Route path='/easter-egg' element={<Easteregg />} />
           <Route path='/chat' element={<ChatroomList />} />
+          <Route path='/chat/:chatroomId' element={<ChatPage />} />
+          
+          {/* 리뷰, 대여기록 */}
           <Route path='/myreview' element={<MyReview />} />
           <Route path='/mysales' element={<MySales />} />
           <Route path='/mypurchase' element={<MyPurchase />} />
           <Route path='/product-review/:id' element={<WriteProductReview />} />
           <Route path='user-review/:id' element={<WriteUserReview />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path='/product/register' element={<ProductRegister />} /> {/* 상품 등록 페이지 */}
-          <Route path='/product/:productId' element={<ProductDetail />} /> {/* 상품 상세 페이지 */}
+          <Route path='/set-to-rented' element={<SetToRented />} />
 
+          {/* 회원 */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/profile-edit' element={<ProfileEdit />} />
+
+          {/* 상품 */}
+          <Route path='/products/register' element={<ProductRegister />} /> {/* 상품 등록 페이지 */}
+          <Route path='/products/:productId' element={<ProductDetail />} /> {/* 상품 상세 페이지 */}
+          <Route path='/products/:productId/edit' element={<ProductEdit />} />{/* 상품 수정 페이지 */}
         </Route>
       </Routes>
     </BrowserRouter>
