9d498fbd7e3daa581199cae4cc24e869c4817aa1
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-01 14:51:47 +0900

    update: path 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import MyPurchase from './pages/MyPurchase';
 import ProductRegister from '@/pages/product/ProductRegister';
 import WriteProductReview from './pages/WriteProductReview';
 import WriteUserReview from './pages/WriteUserReview.jsx';
-import Login from '@/pages/Login.jsx';  // 로그인 페이지 경로
+import Login from '@/pages/Login.jsx'; // 로그인 페이지 경로
 import Signup from '@/pages/Signup.jsx'; // 회원가입 페이지 경로
 import ProductDetail from '@/pages/product/ProductDetail';
 
@@ -20,18 +20,17 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/easter-egg' element={<Easteregg />} />
-          <Route path='/chat' element={<ChatroomList />} />
-          <Route path='/chat/:chatroomId' element={<ChatPage />} />
+          <Route path='/chats' element={<ChatroomList />} />
+          <Route path='/chat' element={<ChatPage />} />
           <Route path='/myreview' element={<MyReview />} />
           <Route path='/mysales' element={<MySales />} />
           <Route path='/mypurchase' element={<MyPurchase />} />
           <Route path='/product-review/:id' element={<WriteProductReview />} />
           <Route path='user-review/:id' element={<WriteUserReview />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
           <Route path='/product/register' element={<ProductRegister />} /> {/* 상품 등록 페이지 */}
           <Route path='/product/:productId' element={<ProductDetail />} /> {/* 상품 상세 페이지 */}
-
         </Route>
       </Routes>
     </BrowserRouter>
