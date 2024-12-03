231c7ed6e83717d790d1c22e2b4f8ccda09626bb
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-03 13:15:21 +0900

    hotfix: axios export 시 문제 발생 수정
    - export default와 export const가 혼재 -> 이를 수정

--- src/components/chatting/ChatItem.jsx
+++ src/components/chatting/ChatItem.jsx
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types';
 
 import '@/styles/chatting/ChatItem.css';
 
-const ChatItem = ({ message, createdAt, isMine }) => {
+const ChatItem = ({ message, createdAt, isMine, isRead }) => {
   return (
     <div className={`message ${isMine ? 'my-message' : 'other-message'}`}>
       <div className='message-time'>{createdAt}</div>
-      <span>{message}</span>
+      <span>
+        {message}
+        {isRead ? '읽음' : ''}
+      </span>
     </div>
   );
 };
@@ -14,6 +17,7 @@ ChatItem.propTypes = {
   isMine: PropTypes.bool,
   message: PropTypes.string,
   createdAt: PropTypes.string,
+  isRead: PropTypes.bool,
 };
 
 export default ChatItem;
