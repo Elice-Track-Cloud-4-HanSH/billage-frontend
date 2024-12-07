64dbe38a54f525c7c06d0711eb4be0c749b862a8
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-07 23:34:14 +0900

    feat: stomp client 초기화 로직 추가
    - 로그인 후 stomp client 연결
    - 로그인 후 자신의 읽지 않은 전체 채팅 개수 조회 및 상태관리에 추가

--- src/pages/AfterLogin.jsx
+++ src/pages/AfterLogin.jsx
@@ -2,16 +2,25 @@ import { useNavigate } from 'react-router-dom';
 import { axiosCredential } from '../utils/axiosCredential';
 import useAuth from './../hooks/useAuth';
 import { useEffect } from 'react';
+import useStompClient from '../storage-provider/zustand/useStompClient';
+import useUnreadChatCount from '../storage-provider/zustand/useUnreadChatCount';
 
 const AfterLogin = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
+  const { connectClient } = useStompClient();
+  const { initUnreadChatCounts } = useUnreadChatCount();
 
   useEffect(() => {
     const getUserInfo = async () => {
       try {
-        const response = await axiosCredential.post('/api/users/after-login');
+        let response = await axiosCredential.post('/api/users/after-login');
         login(response.data);
+        connectClient(response.data.userId);
+
+        response = await axiosCredential.get('/api/chatroom/unread-chat');
+        initUnreadChatCounts(response.data.unreadCount);
+
         navigate('/products', { replace: true });
       } catch (_) {
         navigate('/login', { replace: true });
