1888c9cb1d6e51c3537c03d15ceef99950b8dde3
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-07 23:31:18 +0900

    update: 브라우저 로딩 시 stomp client를 연결하도록 수정

--- src/storage-provider/login-info/AuthProvider.jsx
+++ src/storage-provider/login-info/AuthProvider.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 import AuthContext from './AuthContext';
+import useStompClient from '../zustand/useStompClient';
 
 const AuthProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
+  const { connectClient } = useStompClient();
 
   useEffect(() => {
     const storedUserInfo = localStorage.getItem('userInfo');
     if (storedUserInfo) {
       const decodedUserInfo = JSON.parse(storedUserInfo);
       setUserInfo(decodedUserInfo);
+      connectClient(decodedUserInfo.userId);
     }
   }, []);
 
